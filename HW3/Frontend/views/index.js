96969d39493d40332196d084c3223e6143c8895b
mriver1 <36418396+mriver1@users.noreply.github.com>
2019-03-29 16:02:29 -0400

    Update index.js

--- HW3/Frontend/views/index.js
+++ HW3/Frontend/views/index.js
@@ -1,5 +1,5 @@
 import { default as Home } from './HW3';
 import { default as CurrentWeather } from './Weather';
-import { default as Button } from './Button';
 
-export { HW3, Weather, Button };
+
+export { HW3, Weather };
