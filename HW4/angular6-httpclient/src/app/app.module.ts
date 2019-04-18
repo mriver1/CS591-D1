6516649206a3b5715d38062d24535a15a7d7e9e2
Miranda Rivera-Lynch <mirandarivera-lynch@Mirandas-MacBook-Pro.local>
2019-04-18 16:48:14 -0400

    working files

---
+++ HW4/angular6-httpclient/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { ProductAddComponent } from './product-add/product-add.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductComponent,
+    ProductAddComponent,
+    ProductDetailComponent,
+    ProductEditComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
